1f83b97a964feadca401dea592f7d6fcd5d2410c
Polly <riviera.koder@gmail.com>
2024-06-20 13:47:33 +0200

    jsPersonaliaStringsUpdate

--- js/personalia.js
+++ js/personalia.js
@@ -1,6 +1,8 @@
 //Character info: Gender, pronouns, age, race, origin/birthplace and sexuality
 
-let age = ["10", "13", "15", "18", "20", "23", "28", "33", "38"];
 let gender = ["Female", "Male", "Trans-Female", "TransMale", "Non-Binary"];
 let pronouns = ["She/her", "He/Him", "They/Them", "She/They", "She/Them", "He/They", "He/Them", "She/He/Them"];
-let race = ["Black", "Asian", "Hispanic", "Caucasian", "Native American", "Native Alaskan"];
+let age = ["10", "13", "15", "18", "20", "23", "28", "33", "38"];
+let race = ["Black", "Asian", "Hispanic", "Caucasian", "Native American", "Native Alaskan", "Pacific Islander"];
+let origin = ["African", "North-African", "South-African", "Asian", "Antarctica", "American", "North-American", "South-American", "European"];
+let sexuality = ["Hetero", "Gay", "Bi", "Pan", "Demi", "Ace", "Queer"];
